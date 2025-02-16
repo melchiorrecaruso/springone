864e2e2e69294ef39521869a386fb5389cb9c3b1
Melchiorre Caruso <melchiorrecaruso@gmail.com>
2025-02-16 11:39:44 +0100

    Updating labeling

--- src/aboutfrm.pas
+++ src/aboutfrm.pas
@@ -1,6 +1,6 @@
 { EN13906-1 Helical Compression Spring Designer
 
-  Copyright (C) 2022-2024 Melchiorre Caruso <melchiorrecaruso@gmail.com>
+  Copyright (C) 2022-2025 Melchiorre Caruso <melchiorrecaruso@gmail.com>
 
   This source is free software; you can redistribute it and/or modify it under
   the terms of the GNU General Public License as published by the Free
@@ -37,8 +37,8 @@ type
     AboutDescriptionLabel: TLabel;
     AboutLicenseLabel: TLabel;
     AboutLinkLabel: TLabel;
-    AboutNameLabel: TLabel;
     AboutVersionLabel: TLabel;
+    AboutBuildLabel: TLabel;
     Image: TImage;
     procedure AboutLinkLabelClick(Sender: TObject);
     procedure AboutLinkLabelMouseLeave(Sender: TObject);
@@ -80,20 +80,20 @@ end;
 
 procedure TAboutForm.FormCreate(Sender: TObject);
 begin
-  AboutForm.Top    := ClientFile.ReadInteger('AboutForm', 'Top',    AboutForm.Top);
-  AboutForm.Left   := ClientFile.ReadInteger('AboutForm', 'Left',   AboutForm.Left);
+  AboutForm.Top    := ClientFile.ReadInteger('AboutForm', 'Top', AboutForm.Top);
+  AboutForm.Left   := ClientFile.ReadInteger('AboutForm', 'Left', AboutForm.Left);
   AboutForm.Height := ClientFile.ReadInteger('AboutForm', 'Height', AboutForm.Height);
-  AboutForm.Width  := ClientFile.ReadInteger('AboutForm', 'Width',  AboutForm.Width);
+  AboutForm.Width  := ClientFile.ReadInteger('AboutForm', 'Width', AboutForm.Width);
 end;
 
 procedure TAboutForm.FormClose(Sender: TObject; var CloseAction: TCloseAction);
 begin
   if Windowstate = wsNormal then
   begin
-    ClientFile.WriteInteger('AboutForm', 'Top',    AboutForm.Top);
-    ClientFile.WriteInteger('AboutForm', 'Left',   AboutForm.Left);
+    ClientFile.WriteInteger('AboutForm', 'Top', AboutForm.Top);
+    ClientFile.WriteInteger('AboutForm', 'Left', AboutForm.Left);
     ClientFile.WriteInteger('AboutForm', 'Height', AboutForm.Height);
-    ClientFile.WriteInteger('AboutForm', 'Width',  AboutForm.Width);
+    ClientFile.WriteInteger('AboutForm', 'Width', AboutForm.Width);
   end;
 end;
 
