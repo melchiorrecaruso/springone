3a655dd020959509573ef893b6179c2bfc377133
Melchiorre Caruso <melchiorrecaruso@gmail.com>
2026-01-24 08:15:20 +0100

    Update springtolerances.pas

--- libsrc/springtolerances.pas
+++ libsrc/springtolerances.pas
@@ -202,11 +202,11 @@ const
     (DmMin:160.0; DmMax:200.0; Q1W48:1.20; Q1W814:1.50; Q1W1420:1.70; Q2W48:2.10; Q2W814:2.90; Q2W1420:3.30; Q3W48:4.20; Q3W814:5.70; Q3W1420:6.60));
 
 const
-  MinWireDiameter : TQuantity = ({$IFNDEF ADIMOFF} FID: MeterId; FValue: 0.07/1000 {$ELSE} 0.07/1000 {$ENDIF});
-  MaxWireDiameter : TQuantity = ({$IFNDEF ADIMOFF} FID: MeterId; FValue:   16/1000 {$ELSE}   16/1000 {$ENDIF});
-  MinCoilDiameter : TQuantity = ({$IFNDEF ADIMOFF} FID: MeterId; FValue: 0.63/1000 {$ELSE} 0.63/1000 {$ENDIF});
-  MaxCoilDiameter : TQuantity = ({$IFNDEF ADIMOFF} FID: MeterId; FValue:  200/1000 {$ELSE}  200/1000 {$ENDIF});
-  MaxFreeLength   : TQuantity = ({$IFNDEF ADIMOFF} FID: MeterId; FValue:  630/1000 {$ELSE}  630/1000 {$ENDIF});
+  MinWireDiameter : TQuantity = ({$IFNDEF ADIMOFF} FDim: (FKilogram: 0; FMeter: 60; FSecond: 0; FAmpere: 0; FKelvin: 0; FMole: 0; FCandela: 0; FSteradian: 0); FValue: 0.07/1000 {$ELSE} 0.07/1000 {$ENDIF});
+  MaxWireDiameter : TQuantity = ({$IFNDEF ADIMOFF} FDim: (FKilogram: 0; FMeter: 60; FSecond: 0; FAmpere: 0; FKelvin: 0; FMole: 0; FCandela: 0; FSteradian: 0); FValue:   16/1000 {$ELSE}   16/1000 {$ENDIF});
+  MinCoilDiameter : TQuantity = ({$IFNDEF ADIMOFF} FDim: (FKilogram: 0; FMeter: 60; FSecond: 0; FAmpere: 0; FKelvin: 0; FMole: 0; FCandela: 0; FSteradian: 0); FValue: 0.63/1000 {$ELSE} 0.63/1000 {$ENDIF});
+  MaxCoilDiameter : TQuantity = ({$IFNDEF ADIMOFF} FDim: (FKilogram: 0; FMeter: 60; FSecond: 0; FAmpere: 0; FKelvin: 0; FMole: 0; FCandela: 0; FSteradian: 0); FValue:  200/1000 {$ELSE}  200/1000 {$ENDIF});
+  MaxFreeLength   : TQuantity = ({$IFNDEF ADIMOFF} FDim: (FKilogram: 0; FMeter: 60; FSecond: 0; FAmpere: 0; FKelvin: 0; FMole: 0; FCandela: 0; FSteradian: 0); FValue:  630/1000 {$ELSE}  630/1000 {$ENDIF});
   MinActiveCoils  =  2;
   MinSpringIndex  =  4;
   MaxSpringIndex  = 20;
